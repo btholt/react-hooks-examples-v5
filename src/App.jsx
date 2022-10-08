377895f501d2b89bbf1eb6fa0b4a3b425682aa04
btholt <btholt@gmail.com>
2022-10-08 13:05:46 -0700

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <h1>Intermediate React v5</h1>
+        <select>
+          <option value="/">Home</option>
+          <option value="/useRef">useState</option>
+          <option value="/useReducer">useReducer</option>
+          <option value="/useMemo">useMemo</option>
+          <option value="/useCallback">useCallback</option>
+          <option value="/useLayoutEffect">useLayoutEffect</option>
+          <option value="/useId">useId</option>
+          <option value="/others">Others</option>
+        </select>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
